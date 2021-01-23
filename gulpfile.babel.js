7d4bc25bc02fba2fe08f52c7de3556ab93445a15
wyjeon <61363233+wyjeon@users.noreply.github.com>
2021-01-23 18:13:30 +0900

    Update Watching Files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,7 @@ import ws from "gulp-webserver";
 
 const routes = {
   pug: {
+    watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build",
   },
@@ -18,10 +19,14 @@ const clean = () => del(["build/"]);
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereloade: true, poen: true }));
 
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+};
+
 const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
-const postDev = gulp.series([webserver]);
+const postDev = gulp.parallel([webserver, watch]);
 
 export const dev = gulp.series([prepare, assets, postDev]);
